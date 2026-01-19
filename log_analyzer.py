4d039fc2612717c680797fed18efbd444f51f56c
Quentin Malichecq-Laroche <q_malich@live.concordia.ca>
2026-01-18 21:16:31 -0500

    Update log_analyzer.py

--- log_analyzer.py
+++ log_analyzer.py
@@ -1,5 +1,15 @@
+def count_lines(file_path):
+    count = 0
+    with open(file_path, "r") as file:
+        for line in file:
+            count += 1
+    return count
+
+
 def main():
-    pass
+    file_path = "sample.log"
+    total_lines = count_lines(file_path)
+    print(f"Total number of lines: {total_lines}")
 
 
 if __name__ == "__main__":
